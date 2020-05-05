aa7a7e6e4bd3e4b56123599ebbcca79b1d5c934f
pulze <malagodap2@gmail.com>
2020-05-05 13:16:01 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppNavbar/>
+        <Container>
+          <ItemModal/>
+          <ShoppingList/>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://mern_shopping:mernshopping@cluster0-es6tf.mongodb.net/test?retryWrites=true&w=majority'
+};
