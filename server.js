495d6b55e505fc12de773da13ca0127b72006807
pulze <malagodap2@gmail.com>
2020-05-17 19:42:47 +0530

    new changes..

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
